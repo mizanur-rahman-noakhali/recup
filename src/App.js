a4688dba1be4c2667466582013c6409853eadcc3
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-05-29 16:28:21 +0600

    fantamental

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+}
+
+function Nayok(){
+  const NayokStyle = {
+    border: '2px solid purple',
+    margin: '20px'
+  }
+  return (
+    <div style={NayokStyle}>
+      <h1> Ami khol nayok noi</h1>
+      <h3> I Have Done 5 movies</h3>
+    </div>
+  )
 }
 
 export default App;
